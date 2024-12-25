1af7f348625c1df10d796283e0e3acff5f95c8a0
Thang Nguyen <mrwin95@gmail.com>
2024-12-25 08:00:13 -0700

    user test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -11,8 +11,11 @@ import (
 )
 
 func createRandomAccount(t *testing.T) Accounts {
+
+	user := createRandomUser(t)
+
 	arg := CreateAccountParams{
-		Owner:    util.RandomOwner(),
+		Owner:    user.Username,
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
@@ -32,22 +35,7 @@ func createRandomAccount(t *testing.T) Accounts {
 }
 
 func TestCreateAccount(t *testing.T) {
-	arg := CreateAccountParams{
-		Owner:    util.RandomOwner(),
-		Balance:  util.RandomMoney(),
-		Currency: util.RandomCurrency(),
-	}
-
-	account, err := testQueries.CreateAccount(context.Background(), arg)
-
-	require.NoError(t, err)
-	require.NotEmpty(t, account)
-	require.Equal(t, arg.Owner, account.Owner)
-	require.Equal(t, arg.Balance, account.Balance)
-	require.Equal(t, arg.Currency, account.Currency)
-
-	require.NotZero(t, account.ID)
-	require.NotZero(t, account.CreatedAt)
+	createRandomAccount(t)
 }
 
 func TestGetAccount(t *testing.T) {
