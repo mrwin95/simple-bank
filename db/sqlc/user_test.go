f284daf8a471caab5b499dcc71619b8706b7c377
Thang Nguyen <mrwin95@gmail.com>
2024-12-26 04:43:01 -0700

    password hashed

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -9,9 +9,12 @@ import (
 )
 
 func createRandomUser(t *testing.T) Users {
+	hashedPassword, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+
 	arg := CreateUserParams{
 		Username:       util.RandomOwner(),
-		HashedPassword: "secret",
+		HashedPassword: hashedPassword,
 		FullName:       util.RandomOwner(),
 		Email:          util.RandomEmail(),
 	}
