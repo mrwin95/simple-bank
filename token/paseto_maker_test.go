28f6679907d0e9508985cfc728512bb280dfd434
Thang Nguyen <mrwin95@gmail.com>
2024-12-27 11:07:22 -0700

    fix test

---
+++ token/paseto_maker_test.go
@@ -0,0 +1,22 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/mrwin95/simple_bank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+
+	require.NoError(t, err)
+
+	username := util.RandomOwner()
+	duration := time.Minute
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+}
