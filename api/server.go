be68f5356250e3ab30aa5985942541dc3ce263ec
Thang Nguyen <mrwin95@gmail.com>
2024-12-27 04:48:58 -0700

    add token and login

--- api/server.go
+++ api/server.go
@@ -1,24 +1,45 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	db "github.com/mrwin95/simple_bank/db/sqlc"
+	"github.com/mrwin95/simple_bank/token"
+	"github.com/mrwin95/simple_bank/util"
 )
 
 // create a type to hold the server
 
 type Server struct {
-	store  *db.Store
-	router *gin.Engine
+	config     util.Config
+	store      *db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 // NewServer creates a new server
 
-func NewServer(store *db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store *db.Store) (*Server, error) {
+	tokenMaker, err := token.NewJWTMaker(config.TokenSecret)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 
+	server.setupRouter()
+
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
@@ -27,8 +48,6 @@ func NewServer(store *db.Store) *Server {
 	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
-
-	return server
 }
 
 // Start server
